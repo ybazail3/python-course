2c77a11c3cd1b8ba95eff374b0f58d30b46ec79f
yazzy14 <yazminbazail@icloud.com>
2023-10-24 01:45:35 -0700

    started lesson 8 loops

---
+++ lesson08/loops_practice.py
@@ -0,0 +1,5 @@
+# A while loop will execute a block of code while a condition is True
+value = 1
+while value <= 10:
+    print(value)
+    value += 1  # incremting evrrytine its going through the loop
