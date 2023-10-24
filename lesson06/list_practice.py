53fcbea867f0cd94eb3248db7d7c6bb830c74578
yazzy14 <yazminbazail@icloud.com>
2023-10-24 01:36:54 -0700

    Finshed lesson 6 & 7

--- lesson06/list_practice.py
+++ lesson06/list_practice.py
@@ -27,3 +27,97 @@ print(users)  # ['John', 'David', 'Sarah', 'Mary', 'Richard']
 users.extend(["Robert", "Jimmy"])
 # ['John', 'David', 'Sarah', 'Mary', 'Richard', 'Robert', 'Jimmy']
 print(users)
+
+# users.extend(data)
+# print(users) 'John', 'David', 'Sarah', 'Mary', 'Richard', 'Robert', 'Jimmy', 'John', 24, True]
+
+# ['Bob', 'John', 'David', 'Sarah', 'Mary', 'Richard', 'Robert', 'Jimmy'] this will add "Bob" to the begining of the list bc we said 0 index
+users.insert(0, "Bob")
+print(users)
+
+# ['Bob', 'John', 'Mike', 'Alex', 'David', 'Sarah', 'Mary', 'Richard', 'Robert', 'Jimmy'] this added mike and Alex to the second and third position
+users[2:2] = ['Mike', 'Alex']
+print(users)
+
+users[1:3] = ["Bella", "Tim"]
+# ['Bob', 'Bella', 'Tim', 'Alex', 'David', 'Sarah', 'Mary', 'Richard', 'Robert', 'Jimmy']
+print(users)
+
+# Removing Data
+
+users.remove('Bob')
+# ['Bella', 'Tim', 'Alex', 'David', 'Sarah', 'Mary', 'Richard', 'Robert', 'Jimmy'] removed "Bob"
+print(users)
+
+print(users.pop())  # removes Jimmy because pop() method removes the last item
+print(users)
+
+del users[0]  # Removed Bella since it was the first item
+print(users)
+
+# del data
+data.clear()
+print(data)  # Returns an empty list but doesnt delete it fully []
+
+users[1:2] = ["dave"]
+users.sort()
+# ['Alex', 'David', 'Mary', 'Richard', 'Robert', 'Sarah', 'Tim'] sorts alphabetically
+print(users)
+
+# This allows for lower or uppercase to be ordered correctly in alphbetical order
+users.sort(key=str.lower)
+print(users)
+
+nums = [3, 32, 78, 1, 5]
+nums.reverse()
+print(nums)  # Returns the reverse order [5, 1, 78, 32, 3]
+
+# nums.sort(reverse=True)
+# print(nums) # Returns in descending order [78, 32, 5, 3, 1]
+
+# [78, 32, 5, 3, 1] This doesn't change the original list just sorts it!!!
+print(sorted(nums, reverse=True))
+print(nums)  # [5, 1, 78, 32, 3]
+
+# Creating a variable name numscopy to create a copy of a list
+numscopy = nums.copy()
+mynums = list(nums)
+mycopy = nums[:]  # Slicing the full list and copying it
+
+print(numscopy)
+print(mynums)
+mycopy.sort()
+print(mycopy)  # [1, 3, 5, 32, 78] sorted list
+print(nums)
+
+print(type(nums))  # <class 'list'>
+
+myList = list([1, "str", True])
+print(myList)  # [1, 'str', True]
+
+# Tuples
+
+myTuple = tuple(("Yazmin", 24, True))
+
+anotherTuple = (1, 2, 3, 4, 2, 2)
+
+print(myTuple)  # ('Yazmin', 24, True)
+print(type(myTuple))  # <class 'tuple'>
+print(type(anotherTuple))  # <class 'tuple'>
+
+# Cannot change/add/modify values in a Tuple must turn into list and make changes then turn back into Tuple
+newList = list(myTuple)
+newList.append("Doe")
+newTuple = tuple(newList)
+print(newTuple)
+
+# Unpacking Tuple
+
+# Using the * to get the range that is why two got both values of 2 and 3 when the Tuple was (1, 2, 3, 4)
+(one, *two, hey) = anotherTuple
+print(one)  # 1
+print(two)  # [2, 3]
+print(hey)  # 4
+
+# Counting how many occurences are inside the Tuple is this case 2 is inside anotherTuple 3 times!
+print(anotherTuple.count(2))
---
+++ lesson07/dictionaries_practice.py
@@ -0,0 +1,162 @@
+# Dictionaris are used to store values in key : value pairs looks similar to JSON
+band = {
+    "vocals": "Plant",
+    "guitar": "Page"
+}
+
+# Using dict() to create a dictionary
+band2 = dict(vocals="Plant", guitar="Page")
+
+# Both will print {'vocals': 'Plant', 'guitar': 'Page'}
+print(band)
+print(band2)
+
+print(type(band))  # <class 'dict'>
+print(len(band))  # 2 for two key a: value pairs
+
+# Access items
+# Will get the values aligned with the keys
+print(band["vocals"])
+print(band["guitar"])
+
+# List all keys
+print(band.keys())  # dict_keys(['vocals', 'guitar'])
+
+# List all values
+print(band.values())  # dict_values(['Plant', 'Page'])
+
+# List of key/valu pairs as a Tuple
+print(band.items())  # dict_items([('vocals', 'Plant'), ('guitar', 'Page')])
+
+# Verify if a key ixists in a dictionary
+# Using the "in" keyword to check if keys/values are in the dict
+print("guitar" in band)  # True
+print("triangle" in band)  # False
+
+# Change values
+band["vocals"] = " Coverdale"
+band.update({"bass": "JPJ"})
+print(band)  # {'vocals': ' Coverdale', 'guitar': 'Page', 'bass': 'JPJ'}
+
+# Remove items
+print(band.pop("bass"))  # JPJ pop only returns the value that is removed
+print(band)  # {'vocals': ' Coverdale', 'guitar': 'Page'}
+
+band["drums"] = "Bonham"
+print(band)
+
+# this wll return a Tuple and popitem() will remove the last item
+print(band.popitem())  # ('drums', 'Bonham')
+print(band)  # {'vocals': ' Coverdale', 'guitar': 'Page'}
+
+# Delete and Clear
+band["drums"] = "Bonham"
+del band["drums"]  # deletes band dict
+print(band)
+
+# The clear method will return and empty dict
+band2.clear()
+print(band2)
+
+del band2  # will delete band2
+
+# Copy Dictionaries
+
+# Creates a reference
+# Do not create a copy like this
+# band2 = band
+# print("Bady copy!")
+# print(band2)
+# print(band)
+
+# band2["drums"] = "Yazmin"
+# print(band)
+
+# Do this instead!!!
+band2 = band.copy()
+band2["drums"] = "Yazmin"
+print(band)  # {'vocals': ' Coverdale', 'guitar': 'Page'}
+print(band2)  # {'vocals': ' Coverdale', 'guitar': 'Page', 'drums': 'Yazmin'}
+
+# Or use the dict() constructor function
+band3 = dict(band)
+print("Good copy!")
+print(band3)  # {'vocals': ' Coverdale', 'guitar': 'Page'}
+
+# Nested dictionaries
+
+mbr1 = {
+    "name": "Plant",
+    "instrument": "vocals"
+}
+mbr2 = {
+    "name": "Page",
+    "instrument": "guitar"
+}
+band = {
+    "mbr1": mbr1,
+    "mbr2": mbr2
+}
+# {'mbr1': {'name': 'Plant', 'instrument': 'vocals'}, 'mbr2': {'name': 'Page', 'instrument': 'guitar'}}
+print(band)
+print(band["mbr1"]["name"])  # Plant
+
+# Sets
+
+nums = {1, 2, 3, 4}
+
+nums2 = set((1, 2, 3, 4))
+
+print(nums)  # {1, 2, 3, 4}
+print(nums2)  # {1, 2, 3, 4}
+print(type(nums))  # <class 'set'>
+print(type(nums2))  # <class 'set'>
+
+# no duplicates allows in sets
+nums = {1, 2, 3, 4}
+print(nums)
+
+# True is a duplicate of 1, False is duplicate of zero
+nums = {1, True, 2, False, 3, 4, 0}
+# {False, 1, 2, 3, 4}} False came before 0 that is why it is listed first
+print(nums)
+
+# Using "in" keyword
+# Check if value is in the set
+print(2 in nums)  # True
+
+# But you cannot refer to an element in the set with an idez position or a key
+
+# Add a new element to a set
+nums.add(8)
+print(nums)  # {False, 1, 2, 3, 4, 8}
+
+# Add elements from one set to another
+moreNums = {5, 6, 7}
+nums.update(moreNums)
+print(nums)  # {False, 1, 2, 3, 4, 5, 6, 7, 8}
+
+# Can use update() with Lists, Tuples and Dictionaries too
+
+# Merge two sets to create a new set
+one = {1, 2, 3}
+two = {5, 6, 7}
+
+myNewSet = one.union(two)
+print(myNewSet)
+
+# Keep only the duplicate values of the two sets
+# Changing the first set
+one = {1, 2, 3}
+two = {2, 3, 4}
+
+one.intersection_update(two)
+print(one)  # {2, 3}
+
+# Keep everything except the duplicates
+# Changing the first set
+one = {1, 2, 3}
+two = {2, 3, 4}
+
+one.symmetric_difference_update(two)
+print(one)  # {1, 4}
